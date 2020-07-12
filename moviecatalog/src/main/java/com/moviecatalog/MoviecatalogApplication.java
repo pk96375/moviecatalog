9bb79be79324836e7efd3b7eda871ae6abdde25c
Praveen Yadav <pk96375@gmail.com>
2020-07-12 21:32:22 +0530

    changes

--- moviecatalog/src/main/java/com/moviecatalog/MoviecatalogApplication.java
+++ moviecatalog/src/main/java/com/moviecatalog/MoviecatalogApplication.java
@@ -3,7 +3,6 @@ package com.moviecatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,4 +12,5 @@ public class MoviecatalogApplication {
 		SpringApplication.run(MoviecatalogApplication.class, args);
 	}
 
+	
 }
--- moviecatalog/src/main/java/com/moviecatalog/controller/MovieCatalogController.java
+++ moviecatalog/src/main/java/com/moviecatalog/controller/MovieCatalogController.java
@@ -5,15 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.moviecatalog.model.MovieCatalog;
 import com.moviecatalog.service.MovieCatalogService;
 
-@Controller
+@RestController
 public class MovieCatalogController {
 
 	@Autowired
